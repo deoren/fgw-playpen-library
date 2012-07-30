e967a5806f61b2316868357cf133c5591e8778ad
deoren <deoren@users.noreply.github.com>
2012-07-30 00:36:47 +0000

    Adding small utility script to create directory structure to match what it should look like after prepping it per the instructions on my wiki.

---
+++ prep.bat
@@ -0,0 +1,25 @@
+:: $Id$
+:: $HeadURL$
+
+:: Purpose: Setup directory structure just like unzipping the contents of windows_tutorial.zip would do
+
+for %%folders in (
+chapter_1
+chapter_10
+chapter_11
+chapter_12
+chapter_13
+chapter_14
+chapter_15
+chapter_16
+chapter_17
+chapter_18
+chapter_2
+chapter_3
+chapter_4
+chapter_5
+chapter_6
+chapter_7
+chapter_8
+chapter_9
+) mkdir %%folder
